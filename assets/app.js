384dc25fffc75da3d2600161b04dca5a33eaca48
mariyamansar2000 <mariyamansar2000@gmail.com>
2021-10-05 22:33:34 +0530

    Data-types

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,13 @@
-console.log("Hello World")
-console.warn("Hello Warning")
-console.error("Hello error")
+ //string
+ console.log(typeof("Hello World"))
+ console.log(typeof 'd')
+
+ //numbers
+ console.log(typeof 1)
+ console.log(typeof 1.2)
+ console.log(typeof 1112347265638583585)
+ console.log(typeof -1)
+
+ //boolean
+ console.log(typeof true)
+ console.log(typeof false)

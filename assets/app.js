994907deea65305238cc052da263998f249235db
mariyamansar2000 <mariyamansar2000@gmail.com>
2021-10-05 23:16:59 +0530

    Operators

--- assets/app.js
+++ assets/app.js
@@ -1,5 +1,39 @@
-var arr=[1,2,3,4]
-console.log(arr)
-arr.push(5)
-console.log(arr)
-console.log(arr.length)
+//Operators
+
+//Arithmetic 
+var a=2
+var b=10
+console.log("a+b=",a+b)
+console.log("a-b=",a-b)
+console.log("a*b=",a*b)
+console.log("a/b=",a/b)
+console.log("a**b=",a**b)
+console.log("a%b=",a%b)
+
+//Assignment
+b=11
+console.log('b=',b)
+b+=1
+console.log("b=",b)
+b++
+console.log("b=",b)
+
+//Comparison
+var d=10
+var e=2
+var f='10'
+console.log(d>e)
+console.log(d==f)
+console.log(d===f)
+
+//logical
+console.log("true && true ",true&&true);
+console.log("true && false ",true&&false);
+console.log("false && true ",false&&true);
+console.log("false && false ",false&&false);
+
+console.log("true || true ",true||true);
+console.log("true || false ",true||false);
+console.log("false || true ",false||true);
+console.log("false || false ",false||false);
+

a75797789c7e958a7428052d996022820525a0fb
mariyamansar2000 <mariyamansar2000@gmail.com>
2021-10-05 22:44:06 +0530

    5 data types

--- assets/app.js
+++ assets/app.js
@@ -1,13 +1,2 @@
- //string
- console.log(typeof("Hello World"))
- console.log(typeof 'd')
-
- //numbers
- console.log(typeof 1)
- console.log(typeof 1.2)
- console.log(typeof 1112347265638583585)
- console.log(typeof -1)
-
- //boolean
- console.log(typeof true)
- console.log(typeof false)
+var age=null 
+console.log(typeof age)

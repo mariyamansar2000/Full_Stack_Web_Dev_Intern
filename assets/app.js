6c4a6852b8668861c8d08e1d553c1cb840497da8
mariyamansar2000 <mariyamansar2000@gmail.com>
2021-10-08 13:17:41 +0530

    const function

--- assets/app.js
+++ assets/app.js
@@ -1,20 +1,3 @@
-function add(a=0, b=0)
-{
-    console.log('declaration')
-    var sum = a+b
-    return sum
+const greet = name => 'hi ' + name
 
-}
-
-console.log(add(10,20))
-
-var add = function (a=0,b=1)
-{
-    console.log('expression')
-    var sum = a+b
-    return sum
-}
-
-console.log(add(10,20))
-
-
+console.log(greet('Mariyam'))

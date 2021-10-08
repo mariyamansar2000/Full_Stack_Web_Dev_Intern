0753420eda4edadcba27b255e3e978a9f0a4aceb
mariyamansar2000 <mariyamansar2000@gmail.com>
2021-10-08 13:11:01 +0530

    function order

--- assets/app.js
+++ assets/app.js
@@ -1,25 +1,20 @@
 function add(a=0, b=0)
 {
+    console.log('declaration')
     var sum = a+b
     return sum
+
 }
+
+console.log(add(10,20))
 
 var add = function (a=0,b=1)
 {
+    console.log('expression')
     var sum = a+b
     return sum
 }
 
 console.log(add(10,20))
 
-function greet(name='John',age=99)
-{
-    console.log(name,age)
-}
 
-var greet = function(name,age)
-{
-    console.log(name,age)
-}
-
-greet('Mariyam',21)

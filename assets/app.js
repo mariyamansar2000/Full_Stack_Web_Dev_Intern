9c17250fe41419ee65505969e5e3ee09d12088ac
mariyamansar2000 <mariyamansar2000@gmail.com>
2021-10-08 10:42:14 +0530

    DOM API selectors

---
+++ assets/app.js
@@ -0,0 +1,5 @@
+var heading = document.getElementById('heading')
+var para = document.getElementsByClassName('para')
+var tags = document.getElementsByTagName('a')
+var query = document.querySelector('.para')
+console.log(query)

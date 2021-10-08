98385e452f100a225a2224da1f0448d2536504e7
mariyamansar2000 <mariyamansar2000@gmail.com>
2021-10-08 13:31:48 +0530

    callbackfunctin

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,7 @@
-const greet = name => 'hi ' + name
-
-console.log(greet('Mariyam'))
+var arr = ['go to the GYM','Eat Food', 'Clean House']
+var callbackFunction = (element) => {
+    console.log(element)
+}
+arr.forEach((element,index)=>{
+    console.log(element,index)
+})

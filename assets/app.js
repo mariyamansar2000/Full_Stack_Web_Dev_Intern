aeac05de6e56d2b6b7108a20ec50f3e59978226c
mariyamansar2000 <mariyamansar2000@gmail.com>
2021-10-15 10:24:09 +0530

    Functions

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,2 @@
-var arr = ['go to the GYM','Eat Food', 'Clean House']
-arr.forEach((element,index)=>{
-    console.log(element,index)
-})
+const greet = name => 'hi ' + name
+console.log(greet('Mariyam'))
